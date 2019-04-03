3eef9e4c06dd72cb87d21036ecf6247159f788cd
Alfredogomes <alfredommgomes@gmail.com>
2019-04-04 00:32:35 +0100

    Criei estado vuex para guardar o estado da criação da TS + Continuação da interface

--- src/components/tabSel/CriaTabSel.vue
+++ src/components/tabSel/CriaTabSel.vue
@@ -1,89 +1,107 @@
 <template>
-  <v-stepper v-model="stepNo" vertical>
-    <v-stepper-step :complete="stepNo > 1" step="1">
-      Designação
-      <small>Designação da Nova Tabela de Seleção</small>
-    </v-stepper-step>
-
-    <v-stepper-content step="1">
-        <v-flex xs12 sm6 md3>
-            <v-text-field
-                placeholder="Nome da entidade associada ao utilizador"
-            ></v-text-field>
-        </v-flex>
-      <v-btn color="primary" @click="stepNo = 2">Continuar</v-btn>
-    </v-stepper-content>
-
-    <v-stepper-step :complete="stepNo > 2" step="2">Tipo da Tabela de Seleção</v-stepper-step>
-
-    <v-stepper-content step="2">
-        <v-container fluid grid-list-xl>
-            <v-layout wrap align-center>
-                <v-flex xs3>
-                    <v-subheader>Selecionar tipo de Tabela de Seleção:</v-subheader>
+    <v-container grid-list-md fluid>
+        <v-stepper v-model="stepNo" vertical>
+            <v-progress-linear v-model="valorBarra"></v-progress-linear>
+            <v-stepper-step :complete="stepNo > 1" step="1">
+            Designação
+            <small>Designação da Nova Tabela de Seleção</small>
+            </v-stepper-step>
+            <v-stepper-content step="1">
+                <v-flex xs12 sm6 md3>
+                    <v-text-field
+                        placeholder="Nome da entidade associada ao utilizador"
+                        v-model="estado.nome"
+                    ></v-text-field>
                 </v-flex>
-                <v-flex xs12 sm6 d-flex>
+            <v-btn color="primary" @click="stepNo = 2; barra(25)">Continuar</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-step :complete="stepNo > 2" step="2">Tipo da Tabela de Seleção</v-stepper-step>
+
+            <v-stepper-content step="2">
+                <v-container fluid grid-list-xl>
+                    <v-layout wrap align-center>
+                        <v-flex xs3>
+                            <v-subheader>Selecionar tipo de Tabela de Seleção:</v-subheader>
+                        </v-flex>
+                        <v-flex xs12 sm6 d-flex>
+                            <v-select
+                            :items="items"
+                            label="Tipo"
+                            v-model="estado.tipo"
+                            ></v-select>
+                            <v-flex xs12 sm3>
+                                <v-btn medium @click="infoButton=true" v-if="!infoButton" icon color="info">
+                                <v-icon>info</v-icon>
+                                </v-btn>
+                                <v-btn medium v-if="infoButton" color="info" @click="passos(); estado.passoN = stepNo; estado.percent = valorBarra">
+                                    <v-icon left>info</v-icon>Informações sobre os seguintes passos
+                                </v-btn>
+                            </v-flex>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            <v-btn color="primary" @click="stepNo = 2.5; barra(33)" v-if="estado.tipo == 'Pluriorganizacional'">Continuar</v-btn>
+            <v-btn color="primary" @click="stepNo = 3; barra(50)" v-else>Continuar</v-btn>
+            <v-btn flat @click="stepNo = 1; barra(0)" >Voltar</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-step v-if="estado.tipo == 'Pluriorganizacional'" :complete="stepNo > 3" step="2.5">Indique as entidades abrangidas pela TS:</v-stepper-step>
+            <v-stepper-content step="2.5">
+            <v-layout wrap>
+                <v-flex xs10>
                     <v-select
-                    :items="items"
-                    label="Tipo"
-                    ></v-select>
-                    <v-flex xs12 sm3>
-                        <v-btn medium @click="infoButton=true" v-if="!infoButton" icon color="info">
-                        <v-icon>info</v-icon>
-                        </v-btn>
-                        <v-btn medium v-if="infoButton" color="info" @click="passos()">
-                            <v-icon left>info</v-icon>Informações sobre os seguintes passos
-                        </v-btn>
-                    </v-flex>
+                        item-text="label"
+                        item-value="value"
+                        :items="entidades"
+                        label="Selecione uma classe de nível superior"
+                        solo
+                        dense
+                    />
                 </v-flex>
             </v-layout>
-        </v-container>
-      <v-btn color="primary" @click="stepNo = 3">Continuar</v-btn>
-      <v-btn flat @click="stepNo = 1">Voltar</v-btn>
-    </v-stepper-content>
+            <v-btn color="primary" @click="stepNo = 3; barra(50)">Continuar</v-btn>
+            <v-btn flat @click="stepNo = 2; barra(25)">Voltar</v-btn>
+            </v-stepper-content>
 
-    <v-stepper-step :complete="stepNo > 3" step="3">Indique as entidades abrangidas pela TS:</v-stepper-step>
-    <v-stepper-content step="3">
-      <v-layout wrap>
-        <v-flex xs10 @click="barra(60)">
-            <v-select
-                item-text="label"
-                item-value="value"
-                :items="entidades"
-                label="Selecione uma classe de nível superior"
-                solo
-                dense
-            />
-        </v-flex>
-    </v-layout>
-      <v-btn color="primary" @click="stepNo = 4">Continuar</v-btn>
-      <v-btn flat @click="stepNo = 2">Voltar</v-btn>
-    </v-stepper-content>
+            <v-stepper-step :complete="stepNo > 3" step="3">Processos Comuns</v-stepper-step>
+            <v-stepper-content step="3">
+            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+            <v-btn color="primary" @click="stepNo = 4; barra(75)">Continuar</v-btn>
+            <v-btn flat @click="stepNo = 2.5; barra(33)" v-if="estado.tipo == 'Pluriorganizacional'">Voltar</v-btn>
+            <v-btn flat @click="stepNo = 2; barra(25)" v-else>Voltar</v-btn>
+            </v-stepper-content>
 
-    <v-stepper-step :complete="stepNo > 4" step="4">Passo quatro</v-stepper-step>
-    <v-stepper-content step="4">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="stepNo = 5">Continuar</v-btn>
-      <v-btn flat @click="stepNo = 3">Voltar</v-btn>
-    </v-stepper-content>
+            <v-stepper-step :complete="stepNo > 4" step="4">Processos Específicos</v-stepper-step>
+            <v-stepper-content step="4">
+            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+            <v-btn color="primary" @click="stepNo = 5; barra(75)">Continuar</v-btn>
+            <v-btn flat @click="stepNo = 3; barra(50)">Voltar</v-btn>
+            </v-stepper-content>
 
-    <v-btn color="primary" v-if="stepNo > 4">Submeter</v-btn>
-    <v-btn color="primary" v-else>Guardar trabalho</v-btn>
-    <v-btn flat>Cancelar</v-btn>
-
-  </v-stepper>
+            <v-btn color="primary" v-if="stepNo > 4">Submeter</v-btn>
+            <v-btn color="primary" v-else>Guardar trabalho</v-btn>
+            <v-btn flat>Cancelar</v-btn>
+        </v-stepper>
+    </v-container>
 </template>
 
 
 <script>
 import axios from 'axios'
   export default {
+    computed: {
+        estado() {
+            return this.$store.state.criacaoTabSel
+        }
+    },
     data () {
       return {
         stepNo: 1,
         items: ['Organizacional', 'Pluriorganizacional'],
         entidades: [],
         infoButton: false,
+        valorBarra: 0
       }
     },
     methods: {
@@ -106,15 +124,16 @@ import axios from 'axios'
                 console.log(erro);
             }
         },
-        /*info: async function () {
-            try{
-
-            }
-        }*/
+        barra: async function (valor){
+            this.valorBarra = valor;
+        }
     },
     created: function() {
         this.loadEntidades()
-        console.log(this.entidades)
+        if( this.$store.state.criacaoTabSel.passoN > "1") {
+            this.stepNo =  this.$store.state.criacaoTabSel.passoN
+            this.valorBarra =  this.$store.state.criacaoTabSel.percent
+        }
     }
   }
 </script>
