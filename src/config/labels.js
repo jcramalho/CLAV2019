927e7f7cca927b328c8a720e4d7a99ab665be84b
Jose Carlos Ramalho <jcr@di.uminho.pt>
2019-05-03 14:34:07 +0100

    Adicionei o config/labels.

---
+++ src/config/labels.js
@@ -0,0 +1,10 @@
+export const host = "http://clav-test.di.uminho.pt";
+// module.exports.host = "http://localhost:7778";
+
+export const classeCriacaoDonosOps = {
+  remover: "Remover"
+};
+
+export const classeCriacaoProcessosRelacionadosOps = {
+  remover: "Remover"
+};
