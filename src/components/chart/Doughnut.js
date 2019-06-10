77bc2c05d8a6e8e2eb028e2488ba93deb1ef3288
Octávio Maia <octaviojmaia@gmail.com>
2019-06-10 20:17:04 +0100

    Adicionado grafico donut sobre nivels de utilizador na plataforma

--- src/components/chart/Doughnut.js
+++ src/components/chart/Doughnut.js
@@ -1,21 +1,52 @@
-import {Doughnut} from 'vue-chartjs'
+import { Doughnut } from "vue-chartjs";
+const lhost = require("@/config/global").host;
+import axios from "axios";
 
-export default{
+export default {
   extends: Doughnut,
-  mounted () {
-    this.renderChart({
-      labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
-      datasets: [
-        {
-          backgroundColor: [
-            '#41B883',
-            '#E46651',
-            '#00D8FF',
-            '#DD1B16'
-          ],
-          data: [40, 20, 80, 10]
+  data() {
+    return {
+      info: {
+        labels: [
+          "Administrador de Perfil Tecnológico",
+          "Administrador de Perfil Funcional",
+          "Utilizador Validador",
+          "Utilizador Avançado",
+          "Utilizador Decisor",
+          "Utilizador Simples",
+          "Representante Entidade"
+        ],
+        datasets: [
+          {
+            backgroundColor: [
+              "#00876c",
+              "#63b179",
+              "#aed987",
+              "#ffff9d",
+              "#fcc267",
+              "#ef8250",
+              "#d43d5"
+            ],
+            data: [0, 0, 0, 0, 0, 0, 0]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  async mounted() {
+    await axios
+      .get(lhost + "/api/users?formato=normalizado")
+      .then(res => {
+        for (var i = 0; i < res.data.length; i++) {
+          var index = this.info.labels.indexOf(res.data[i].level);
+          this.info.datasets[0].data[index] += 1;
         }
-      ]
-    }, {responsive: true, maintainAspectRatio: false})
+        this.renderChart(this.info, this.options);
+      })
+      .catch(error => alert(error));
   }
-}
+};
