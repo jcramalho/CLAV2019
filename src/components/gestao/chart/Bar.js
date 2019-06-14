c453a0dde7f0f4a00a5e2b4b1fec647b94ab13c7
Octávio Maia <octaviojmaia@gmail.com>
2019-06-14 16:20:14 +0100

    Agora permite edicao de entidade e conta PUTs no gráfico

--- src/components/gestao/chart/Bar.js
+++ src/components/gestao/chart/Bar.js
@@ -17,6 +17,11 @@ export default {
           },
           {
             label: "POST",
+            backgroundColor: "#aed987",
+            data: []
+          },
+          {
+            label: "PUT",
             backgroundColor: "#05CBE1",
             data: []
           }
@@ -39,6 +44,7 @@ export default {
             var index = this.info.labels.indexOf(res.data[i]._id);
             this.info.datasets[0].data[index] = res.data[i].nCallsGet;
             this.info.datasets[1].data[index] = res.data[i].nCallsPost;
+            this.info.datasets[2].data[index] = res.data[i].nCallsPut;
           }
         }
         this.renderChart(this.info, this.options);
