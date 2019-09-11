60e770411c4503621b26b5fd1dd1eaad3a07455e
jcm300 <zeclmartins@gmail.com>
2019-09-11 20:58:46 +0100

    Adicionados botões no importar CSV

--- src/components/tabSel/importarTS/ImportaTSCSV.vue
+++ src/components/tabSel/importarTS/ImportaTSCSV.vue
@@ -1,18 +1,59 @@
 <template>
-  <v-layout row wrap ma-2>
-      <v-flex xs2>
-        <div class="info-label">Importar CSV:</div>
-      </v-flex>
-      <v-flex xs9>
-        <v-file-input multiple label="File input"></v-file-input>
-      </v-flex>
-    </v-layout>
+  <v-layout row wrap ma-2 justify-center>
+    <v-flex xs9 class="display-1">
+      <v-file-input
+        v-model="file"
+        label="Importar CSV"
+        placeholder="Escolha o seu ficheiro"
+        :show-size="1000"
+        accept="text/csv"
+      ></v-file-input>
+      <div class="text-center">
+        <v-btn
+          class="white--text ma-2"
+          color="indigo darken-4"
+          @click="cancelar()"
+        >
+          Cancelar
+        </v-btn>
+        <v-btn
+          class="white--text ma-2"
+          color="indigo darken-4"
+          @click="enviarFicheiro()"
+          :disabled="file.length == 0"
+        >
+          Enviar
+        </v-btn>
+      </div>
+      <v-snackbar
+        v-model="sended"
+        :timeout="4000"
+        color="success"
+        :top="true"
+      >
+        {{ text }}
+        <v-btn flat @click="fecharSnackbar">Fechar</v-btn>
+      </v-snackbar>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-
+import axios from "axios";
 
 export default {
-  
+  data: () => ({
+    file: [],
+    sended: true
+  }),
+
+  methods: {
+    enviarFicheiro: function() {
+
+    },
+    cancelar: function() {
+      this.$router.push("/");
+    }
+  }
 };
 </script>
