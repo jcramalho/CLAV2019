2f8924cc7df96b9a27506d26c5a8c393d58cc99b
jcm300 <zeclmartins@gmail.com>
2019-09-16 00:35:29 +0100

    Agora a interface apresenta erros e sucesso através de dialogs

--- src/components/tabSel/importarTS/ImportaTSCSV.vue
+++ src/components/tabSel/importarTS/ImportaTSCSV.vue
@@ -1,12 +1,53 @@
 <template>
   <v-layout row wrap ma-2 justify-center>
     <v-flex xs9>
-      <v-alert :value="erro != ''" type="error">
-        {{ erro }}
-      </v-alert>
-      <v-alert :value="success != ''" type="success">
-        {{ success }}
-      </v-alert>
+      <v-dialog v-model="erroDialog" width="700">
+        <v-card color="error">
+          <v-card-title class="headline font-weight-light">
+            Não foi possível criar o pedido de criação de tabela de seleção a
+            partir do ficheiro importado...
+          </v-card-title>
+
+          <v-card-text>
+            <span class="subtitle-1" style="white-space: pre-wrap">
+              {{ erro }}
+            </span>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-btn color="indigo darken-4" text @click="erroDialog = false">
+              Fechar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="successDialog" width="700">
+        <v-card color="success">
+          <v-card-title class="headline font-weight-light">
+            Pedido de criação de tabela de seleção criado com sucesso a partir
+            do ficheiro importado.
+          </v-card-title>
+
+          <v-card-text>
+            <span
+              class="subtitle-1"
+              style="white-space: pre-wrap"
+              v-html="success"
+            >
+            </span>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-btn color="indigo darken-4" text @click="successDialog = false">
+              Fechar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-file-input
         v-model="file"
         label="Importar CSV/Excel"
@@ -46,7 +87,9 @@ export default {
   data: () => ({
     file: null,
     erro: "",
+    erroDialog: false,
     success: "",
+    successDialog: false,
     loading: false
   }),
 
@@ -54,6 +97,8 @@ export default {
     enviarFicheiro: async function() {
       try {
         this.erro = "";
+        this.erroDialog = false;
+        this.successDialog = false;
         this.success = "";
         this.loading = true;
         var formData = new FormData();
@@ -70,12 +115,66 @@ export default {
           formData
         );
         this.loading = false;
-        this.success = `Pedido de criação de tabela de seleção criado com sucesso a partir do ficheiro importado. Código do pedido: ${
-          response.data
-        }`;
+
+        var stats = "<ul>";
+        for (var k in response.data.stats) {
+          switch (k) {
+            case "processos":
+              stats +=
+                "<li>Número de Processos: " + response.data.stats[k] + "</li>";
+              break;
+            case "donos":
+              stats +=
+                "<li>Número de Processos Donos: " +
+                response.data.stats[k] +
+                "</li>";
+              break;
+            case "participantes":
+              stats +=
+                "<li>Número de Processos Participantes: " +
+                response.data.stats[k] +
+                "</li>";
+              break;
+            default:
+              stats += "<li>Entidade: " + k + "<ul>";
+              for (var kb in response.data.stats[k]) {
+                switch (kb) {
+                  case "processos":
+                    stats +=
+                      "<li>Número de Processos: " +
+                      response.data.stats[k][kb] +
+                      "</li>";
+                    break;
+                  case "donos":
+                    stats +=
+                      "<li>Número de Processos Donos: " +
+                      response.data.stats[k][kb] +
+                      "</li>";
+                    break;
+                  case "participantes":
+                    stats +=
+                      "<li>Número de Processos Participantes: " +
+                      response.data.stats[k][kb] +
+                      "</li>";
+                    break;
+                  default:
+                    break;
+                }
+              }
+              stats += "</ul>";
+          }
+        }
+        stats += "</ul>";
+
+        this.success = `Código do pedido: ${
+          response.data.codigo
+        }\nEstatísticas:\n${stats}`;
+
+        this.successDialog = true;
       } catch (e) {
         this.loading = false;
-        this.erro = "Não foi possível importar o CSV/Excel... Tente novamente.";
+        this.erro = e.response.data;
+        this.erroDialog = true;
       }
     },
     cancelar: function() {
