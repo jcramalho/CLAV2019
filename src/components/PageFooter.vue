7ce99ed8d44adbbe2699f7cc1dc655a893fdbed1
129Camal <frederico21pinto@hotmail.com>
2019-09-23 11:37:06 +0100

    Atualização do Footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-app-bar color="indigo darken-4" dark dense>
+  <v-app-bar color="indigo darken-4" dark dense height="80px">
     <v-toolbar-title>DGLAB - Direção-Geral do Livro, dos Arquivos e das Bibliotecas</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-title>
-      <span><a href="mailto:clav@dglab.gov.pt">Contactos</a></span>
+      <v-btn text depressed href="mailto:clav@dglab.gov.pt">Contactos</v-btn>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-img :src="require('./../assets/feder.png')" max-height="60px" />
+    <v-img :src="require('./../assets/feder.png')" aspect-ratio="4.8600" />
   </v-app-bar>
 </template>
+
+<style scoped>
+.v-btn:hover:before {
+  opacity: 0;
+}
+</style>
