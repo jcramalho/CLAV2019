887238eb7cdb2fc24c6818c634d99fd3218d9497
jcm300 <zeclmartins@gmail.com>
2019-10-10 13:29:31 +0100

    Registo de acesso já apresenta os acessos

--- src/components/users/RegistoAcesso.vue
+++ src/components/users/RegistoAcesso.vue
@@ -1,44 +1,25 @@
 <template>
-  <v-container fluid fill-height>
-    <v-row align-center justify-center>
-      <v-col cols="12">
-        <v-data-table
-          :headers="headersUsers"
-          :items="users"
-          item-key="_id"
-          :single-expand="true"
-          show-expand
-          class="elevation-1"
-        >
-          <template v-slot:top>
-            <v-toolbar flat color="indigo darken-4" dark>
-              <v-toolbar-title>Registo de acesso</v-toolbar-title>
-            </v-toolbar>
-          </template>
-          <template v-slot:expanded-item="{ item, headers }">
-            <td :colspan="headers.length" class="pa-1">
-              <v-data-table
-                :headers="headersRoutes"
-                :items="item.logs"
-                item-key="id"
-                :single-expand="true"
-                show-expand
-                class="elevation-1"
-              >
-                <template v-slot:expanded-item>
-                  <td :colspan="headersUsers.length">Peek-a-boo!</td>
-                </template>
-              </v-data-table>
-            </td>
-          </template>
-        </v-data-table>
-        <v-snackbar :value="text != ''" :color="color" :top="true">
-          {{ text }}
-          <v-btn text @click="text = ''">Fechar</v-btn>
-        </v-snackbar>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row align-center justify-center>
+    <v-col cols="12">
+      <v-data-table
+        :headers="headers"
+        :items="logs"
+        item-key="_id"
+        class="elevation-1 ma-10"
+        show-group-by
+      >
+        <template v-slot:top>
+          <v-toolbar flat color="indigo darken-4" dark>
+            <v-toolbar-title>Registo de acesso</v-toolbar-title>
+          </v-toolbar>
+        </template>
+      </v-data-table>
+      <v-snackbar :value="text != ''" :color="color" :top="true">
+        {{ text }}
+        <v-btn text @click="text = ''">Fechar</v-btn>
+      </v-snackbar>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -46,14 +27,30 @@ export default {
   mounted: async function() {
     try {
       var response = await this.$request("get", "/api/users");
-      this.users = response.data;
+      this.users = {};
 
-      for (var i = 0; i < this.users.length; i++) {
-        response = await this.$request(
-          "get",
-          "/api/calls/User/" + this.users[i]._id
-        );
-        this.users[i].logs = response.data;
+      for (var i = 0; i < response.data.length; i++) {
+        this.users[response.data[i]._id] = response.data[i].email;
+      }
+
+      response = await this.$request("get", "/api/chaves/listagem");
+      this.keys = {};
+
+      for (i = 0; i < response.data.length; i++) {
+        this.keys[response.data[i].id] = response.data[i].contactInfo;
+      }
+
+      response = await this.$request("get", "/api/calls");
+      this.logs = response.data;
+
+      for (i = 0; i < this.logs.length; i++) {
+        if (this.logs[i].type == "Chave") {
+          this.logs[i].email = this.keys[this.logs[i].id];
+        } else {
+          this.logs[i].email = this.users[this.logs[i].id];
+        }
+
+        this.logs[i].accessDate = new Date(this.logs[i].accessDate).toLocaleString();
       }
     } catch (error) {
       this.color = "error";
@@ -61,16 +58,16 @@ export default {
     }
   },
   data: () => ({
-    users: [],
-    headersUsers: [
-      { text: "Nome", align: "center", value: "name" },
+    users: {},
+    keys: {},
+    logs: [],
+    headers: [
       { text: "Email", align: "center", value: "email" },
-      { text: "", value: "data-table-expand" }
-    ],
-    headersRoutes: [
+      { text: "Tipo", align: "center", value: "type" },
       { text: "Rota", align: "center", value: "route" },
       { text: "Método", align: "center", value: "method" },
-      { text: "", value: "data-table-expand" }
+      { text: "Status HTTP", align: "center", value: "httpStatus" },
+      { text: "Acedido em", align: "center", value: "accessDate" }
     ],
     color: "",
     text: ""
