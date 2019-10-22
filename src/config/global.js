24affd1253ee8b7f5ee8718691d1f9a0af72fadd
José Carlos Ramalho <jcr@iMac-de-iMac-2.lan>
2019-10-22 20:37:11 +0100

    Configs...

--- src/config/global.js
+++ src/config/global.js
@@ -1,3 +1,3 @@
-module.exports.host = 'http://clav-test.di.uminho.pt'
+//module.exports.host = 'http://clav-test.di.uminho.pt'
 //module.exports.host = 'http://localhost:7779'
-//module.exports.host = window.location.protocol + "//" + window.location.host //para o caso do nginx proxy
+module.exports.host = window.location.protocol + "//" + window.location.host //para o caso do nginx proxy
