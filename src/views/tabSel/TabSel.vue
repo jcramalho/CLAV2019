9966b35ce68a76d8d0a5dff631d2830ee69d49aa
José Carlos Ramalho <jcr@iMac-de-iMac-2.lan>
2019-11-07 18:35:01 +0000

    Listagem de TS.

--- src/views/tabSel/TabSel.vue
+++ src/views/tabSel/TabSel.vue
@@ -1,12 +1,41 @@
 <template>
-  <v-card class="ma-8">
-    <v-card-title class="indigo darken-4 white--text" dark
-      >Lista de Tabelas de Seleção</v-card-title
-    >
-    <v-card-text>
-      <p class="pa-4 subtitle-2">
-        De momento, ainda não há Tabelas de Seleção carregadas no sistema.
-      </p>
-    </v-card-text>
-  </v-card>
+  <div>
+    <Loading v-if="!fontesReady" :message="'fontes de legitimação'" />
+    <Listagem v-else :lista="fontes" tipo="Fontes de Legitimação" :cabecalho="cabecalhos" :campos="campos" />
+  </div>
 </template>
+<script>
+import Listagem from "@/components/generic/Listagem.vue"; // @ is an alias to /src
+import Loading from "@/components/generic/Loading";
+
+import { NIVEL_MINIMO_ALTERAR } from "@/utils/consts";
+
+export default {
+  data: () => ({
+    fontes: [],
+    campos: [],
+    cabecalhos: [],
+    fontesReady: false
+  }),
+
+  components: {
+    Listagem,
+    Loading
+  },
+
+  created: async function() {
+    try {
+      let response = await this.$request("get", "/api/legislacao?fonte=PGD");
+
+      this.cabecalhos = ["Data", "Tipo", "Número", "Sumário", "Acesso"];
+      this.campos = ["data", "tipo", "numero", "sumario", "link"];
+
+      this.fontes = response.data
+      this.fontesReady = true;
+    } 
+    catch (e) {
+      return e;
+    }
+  }
+};
+</script>
