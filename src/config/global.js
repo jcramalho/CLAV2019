0bbff5a14050ea4225479a45435992ba49195563
jcm300 <zeclmartins@gmail.com>
2020-02-19 18:28:13 +0000

    Pequenas alterações por forma a separar a interface da futura API Gateway

--- src/config/global.js
+++ src/config/global.js
@@ -1,6 +1,6 @@
-module.exports.host = "http://clav-test.di.uminho.pt";
-// module.exports.host = "http://localhost:7779";
-//module.exports.host = 'http://localhost:8080' //para o nginx proxy quando a interface é local e está em localhost:8080
-//module.exports.host = "http://clav.dglab.gov.pt"; //para o nginx proxy quando a interface não é local
-// module.exports.host = 'http://epl.di.uminho.pt:7781'
-//module.exports.host = window.location.protocol + "//" + window.location.host //para o caso do nginx proxy (ainda não funciona a 100%, usar uma das anteriores)
+//module.exports.host = process.env.API_URL || "http://clav-test.di.uminho.pt";
+module.exports.host = process.env.API_URL || "http://localhost:7779";
+//module.exports.host = process.env.API_URL || 'http://localhost:8080' //para o nginx proxy quando a interface é local e está em localhost:8080
+//module.exports.host = process.env.API_URL || "http://clav.dglab.gov.pt"; //para o nginx proxy quando a interface não é local
+//module.exports.host = process.env.API_URL || 'http://epl.di.uminho.pt:7781'
+//module.exports.host = process.env.API_URL || window.location.protocol + "//" + window.location.host //para o caso do nginx proxy (ainda não funciona a 100%, usar uma das anteriores)
