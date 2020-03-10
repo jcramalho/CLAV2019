d3dc2b0c5e34bbaaae203c4c265318dc4d0c8a32
jcm300 <zeclmartins@gmail.com>
2020-03-10 22:53:17 +0000

    HTTPS já funcional sem reverse proxy, faltam alguns retoques, como melhorar o DHE parameter

---
+++ prepareHTTPS.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#Used acme.sh script from https://github.com/acmesh-official/acme.sh
+#necessary packages: openssl, cron, curl
+
+NAME=acme.sh
+FOLDER=~/.acme.sh
+EXEC=$FOLDER/$NAME
+#DOMAINS=('clav.dglab.gov.pt' 'epl.di.uminho.pt')
+DOMAINS=('clav.ddns.net')
+WEBDIR=/var/www/html
+CERT_FOLDER=/etc/nginx/acme.sh
+
+function join_by {
+    local d=$1
+    shift
+    echo -n "$1"
+    shift
+    printf "%s" "${@/#/$d}"
+}
+
+#Download acme.sh script
+downloadInstallScript() {
+    #Install necessary packages
+    local packages=('openssl' 'curl')
+    local toInstall
+
+    for p in "${packages[@]}"; do
+        which $p 2> /dev/null > /dev/null
+        if [ $? -ne 0 ]; then
+            toInstall+=($p)
+        fi
+    done
+
+    which crontab 2> /dev/null > /dev/null
+    local haveCrontab=$?
+
+    declare -A pkg_mngs
+    pkg_mngs[apk]="apk --no-cache add -f"
+    pkg_mngs[apt-get]="apt-get install -y"
+    pkg_mngs[dnf]="dnf install -y"
+    pkg_mngs[yum]="yum -y install"
+    pkg_mngs[zypper]="zypper install -y"
+    pkg_mngs[pacman]="pacman -S --noconfirm"
+
+    declare -A cron
+    cron[apt-get]="cron"
+    cron[dnf]="crontabs"
+    cron[yum]="crontabs"
+    cron[zypper]="cron"
+    cron[pacman]="cronie"
+
+    local pkg_mng
+    for pm in "${!pkg_mngs[@]}"; do
+        if [ -x "$(command -v $pm)" ]; then
+            pkg_mng="${pkg_mngs[$pm]}"
+
+            if [[ ! -z "${cron[$pm]}" ]] && [[ $haveCrontab -ne 0 ]]; then
+                toInstall+=(${cron[$pm]})
+            fi
+            break
+        fi
+    done
+
+    if [[ ! -z $pkg_mng ]] && [[ ${#toInstall[@]} -gt 0 ]]; then
+        sudo $pkg_mng ${toInstall[@]}
+    fi
+
+    #Check if script not exists
+    if [ ! -f "$EXEC" ]; then
+        curl -O https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh
+        chmod +x $NAME
+        ./$NAME --install
+        rm $NAME
+        $EXEC --upgrade --auto-upgrade
+    fi
+}
+
+getCertificate() {
+    local domains="-d $(join_by ' -d ' ${DOMAINS[@]})"
+    
+    if [ ! -d $WEBDIR ]; then
+        mkdir $WEBDIR
+        #chown -R www-data:www-data $WEBDIR
+    fi
+
+    $EXEC --issue $domains -w $WEBDIR
+}
+
+installCertificate() {
+    if [ ! -d $CERT_FOLDER ]; then
+        mkdir -p $CERT_FOLDER
+    fi
+
+    $EXEC --install-cert -d $DOMAINS \
+        --cert-file $CERT_FOLDER/cert.pem \
+        --key-file $CERT_FOLDER/key.pem \
+        --fullchain-file $CERT_FOLDER/fullchain.pem \
+        --reloadcmd "nginx -s reload"
+}
+
+downloadInstallScript
+getCertificate
+installCertificate
--- src/config/global.js
+++ src/config/global.js
@@ -4,7 +4,7 @@ const dominioDefault = "http://clav-test.di.uminho.pt";
 //const dominioDefault = "http://clav.dglab.gov.pt"; //para o nginx proxy quando a interface não é local
 //const dominioDefault = "http://epl.di.uminho.pt:7781";
 
-const dominio = process.env.API_URL || dominioDefault;
-const apiVersion = process.env.API_VERSION || "v2";
+const dominio = process.env.VUE_APP_API_URL || dominioDefault;
+const apiVersion = process.env.VUE_APP_API_VERSION || "v2";
 
 module.exports.host = dominio + "/" + apiVersion;
