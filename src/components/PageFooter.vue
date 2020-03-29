04b96796f3e88d2b91ef4ea7b89b1b371d8cf895
José Carlos Ramalho <jcr@iMac-de-iMac.lan>
2020-03-29 15:10:02 +0100

    Configs.

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -11,7 +11,7 @@
     </v-toolbar-title>
     <v-spacer></v-spacer>
 
-    <div class="body-2">Versão: 2020-03-25</div>
+    <div class="body-2">Versão: 2020-03-26</div>
 
     <v-spacer></v-spacer>
 
