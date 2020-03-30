98953462f2cfae68604aa829802a73a0ebfc4369
José Carlos Ramalho <jcr@iMac-de-iMac.lan>
2020-03-30 22:03:48 +0100

    Dei início ao tratamento do ShowPedido das Classes.

---
+++ src/components/pedidos/consulta/classes/ShowExemplosNotasAp.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-row>
+        <v-col cols="2">
+          <div class="info-label">Exemplos de notas de aplicação</div>
+        </v-col>
+        <v-col>
+          <div class="info-content">
+              <ul>
+                  <li v-for="(n,i) in lista" :key="i">{{ n.exemplo }}</li>
+              </ul>
+          </div>
+        </v-col>
+      </v-row>
+</template>
+
+<script>
+export default {
+  props: ["lista"]
+}
+</script>
---
+++ src/components/pedidos/consulta/classes/ShowParticipantes.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-row>
+        <v-col cols="2">
+          <div class="info-label">Participantes no processo</div>
+        </v-col>
+        <v-col>
+          <div class="info-content">
+              
+          </div>
+        </v-col>
+      </v-row>
+</template>
+
+<script>
+export default {
+  props: ["lista"]
+}
+</script>
