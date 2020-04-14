ebf99a638679993409ccbe0291c068ac32051cc1
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-04-14 13:40:12 +0100

    correção da página inicial depois do merge com userDashboard

--- src/views/Entidade.vue
+++ src/views/Entidade.vue
@@ -2,9 +2,13 @@
   <div>
     <v-row>
       <v-col>
+        <Info v-if="this.$store.state.name === ''"/>
         <Operacoes :level="level" />
+        <Noticias :level="level" v-if="this.$store.state.name === ''"/>
       </v-col>
       <v-col>
+        <DocumentacaoApoio v-if="this.$store.state.name === ''"/>
+        <Registo v-if="this.$store.state.name === ''"/>
         <Gestao v-if="level > 0" :level="level" />
       </v-col>
     </v-row>
@@ -15,10 +19,18 @@
 /* eslint-disable */
 import Operacoes from "@/components/principal/Operacoes.vue";
 import Gestao from "@/components/principal/Gestao.vue";
+import Noticias from "@/components/principal/Noticias.vue";
+import Info from "@/components/principal/Info.vue";
+import DocumentacaoApoio from "@/components/principal/DocumentacaoApoio.vue";
+import Registo from "@/components/principal/Registo.vue";
 
 export default {
   components: {
+    Info,
+    DocumentacaoApoio,
+    Registo,
     Operacoes,
+    Noticias,
     Gestao
   },
   data() {
