5cd7c4921fc966fc23da878769d3fc4d0513db31
AlexMTeixeira <anguinho@github.com>
2020-04-19 23:16:58 +0100

    AE: Analise de Pedido para Criaçao Concluido

--- src/components/pedidos/validacao/ValidaAE.vue
+++ src/components/pedidos/validacao/ValidaAE.vue
@@ -1,7 +1,224 @@
 <template>
   <div>
-    teste
-    
+    <v-row>
+      <v-col cols="2">
+        <div class="info-label">Fonte de Legitimação:</div>
+      </v-col>
+      <v-col class="mt-3">{{ p.objeto.dados.ae.legislacao }}</v-col>
+      <v-col cols="1">
+        <v-icon color="green" @click="verifica(info)">check</v-icon>
+        <v-icon color="red" @click="anula(info)">clear</v-icon>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="2">
+        <div class="info-label">Fundo:</div>
+      </v-col>
+      <v-col class="mt-3">
+        <li v-for="(f,i) in p.objeto.dados.ae.fundo" :key="i">{{f}}</li>
+      </v-col>
+      <v-col cols="1">
+        <v-icon color="green" @click="verifica(info)">check</v-icon>
+        <v-icon color="red" @click="anula(info)">clear</v-icon>
+      </v-col>
+    </v-row>
+
+    <v-expansion-panels popout>
+      <v-expansion-panel class="ma-1">
+        <v-expansion-panel-header class="pa-2 indigo darken-4 title white--text">Classes</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-list>
+            <v-list-group
+              v-for="item in p.objeto.dados.ae.zonaControlo"
+              :key="item.codigo"
+              color="grey darken-1"
+              no-action
+            >
+              <template v-slot:activator>
+                <v-list-item-content class="info-label">
+                  <v-list-item-title
+                    v-if="item.codigo && item.referencia"
+                    v-text="
+                          'Zona de Controlo - ' +
+                            item.codigo +
+                            ' ' +
+                            item.referencia
+                        "
+                  ></v-list-item-title>
+                  <v-list-item-title
+                    v-else-if="item.codigo"
+                    v-text="'Zona de Controlo - ' + item.codigo"
+                  ></v-list-item-title>
+                  <v-list-item-title v-else v-text="'Zona de Controlo - ' + item.referencia"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-row v-if="item.codigo">
+                    <v-col cols="4">
+                      <div class="info-label">Código da class:</div>
+                    </v-col>
+                    <v-col class="mt-3">
+                      <a
+                        v-if="!item.referencia"
+                        :href="'/classes/consultar/c' + item.codigo"
+                      >{{ item.codigo }}</a>
+                      <div v-else>{{ item.codigo }}</div>
+                    </v-col>
+                    <v-col cols="1">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="item.referencia">
+                    <v-col cols="4">
+                      <div class="info-label">Referência:</div>
+                    </v-col>
+                    <v-col class="mt-3">{{ item.referencia }}</v-col>
+                    <v-col cols="1" v-if="!item.codigo">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="item.titulo">
+                    <v-col cols="4">
+                      <div class="info-label">Título:</div>
+                    </v-col>
+                    <v-col class="mt-3">{{ item.titulo }}</v-col>
+                  </v-row>
+                  <v-row v-if="item.prazoConservacao">
+                    <v-col cols="4">
+                      <div class="info-label">Prazo de Conservação Administrativa:</div>
+                    </v-col>
+                    <v-col class="mt-3">{{ item.prazoConservacao }} Anos</v-col>
+                  </v-row>
+                  <v-row v-if="item.destino">
+                    <v-col cols="4">
+                      <div class="info-label">Destino Final:</div>
+                    </v-col>
+                    <v-col class="mt-3">
+                      <span v-if="item.destino === 'E'">Eliminação</span>
+                      <span v-else-if="item.destino === 'C'">Conservação</span>
+                      <span v-else >{{ item.destino }}</span>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="item.ni">
+                    <v-col cols="4">
+                      <div class="info-label">Natureza de intervenção:</div>
+                    </v-col>
+                    <v-col class="mt-3">{{ item.ni }}</v-col>
+                  </v-row>
+                  <v-row v-if="item.dono">
+                    <v-col cols="4">
+                      <div class="info-label">Dono do PN:</div>
+                    </v-col>
+                    <v-col class="mt-3">
+                      <li v-for="(d,i) in item.dono" :key="i">{{ d }}</li>
+                    </v-col>
+                    <v-col cols="1">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="4">
+                      <div class="info-label">Data de Início:</div>
+                    </v-col>
+                    <v-col class="mt-3">{{ item.dataInicio }}</v-col>
+                    <v-col cols="1">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="4">
+                      <div class="info-label">Data de Fim:</div>
+                    </v-col>
+                    <v-col class="mt-3">{{ item.dataFim }}</v-col>
+                    <v-col cols="1">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="item.uiPapel">
+                    <v-col cols="4">
+                      <div class="info-label">Medição das UI em papel (m.l.):</div>
+                    </v-col>
+                    <v-col class="mt-3">
+                      {{ item.uiPapel }}
+                    </v-col>
+                    <v-col cols="1">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="item.uiDigital">
+                    <v-col cols="4">
+                      <div class="info-label">Medição das UI em digital (Gb):</div>
+                    </v-col>
+                    <v-col class="mt-3">
+                      {{ item.uiDigital }}
+                    </v-col>
+                    <v-col cols="1">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="item.uiOutros">
+                    <v-col cols="4">
+                      <div class="info-label">Medição das UI noutros suportes:</div>
+                    </v-col>
+                    <v-col class="mt-3">
+                      {{ item.uiOutros }}
+                    </v-col>
+                    <v-col cols="1">
+                      <v-icon color="green" @click="verifica(info)">check</v-icon>
+                      <v-icon color="red" @click="anula(info)">clear</v-icon>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="4">
+                      <div class="info-label">N.º de agregações:</div>
+                    </v-col>
+                    <v-col class="mt-3">{{ item.agregacoes.length }}</v-col>
+                  </v-row>
+                  <div class="ma-1">
+                    <v-row justify="space-between" class="info-label">
+                      <v-col>Lista de Agregações</v-col>
+                      <v-col class="mt-3">
+                        <v-text-field
+                          v-model="search"
+                          append-icon="search"
+                          label="Procura"
+                          single-line
+                          hide-details
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-data-table
+                      :headers="cabecalho"
+                      :items="item.agregacoes"
+                      :items-per-page="5"
+                      class="elevation-1 ml-2 mt-3"
+                      :footer-props="footer_props"
+                      :search="search"
+                    />
+                  </div>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-group>
+          </v-list>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels> 
+    <v-row>
+      <v-spacer />
+      <PO
+        operacao="Validar"
+        @finalizarPedido="finalizarPedido($event)"
+        @devolverPedido="despacharPedido($event)"
+      />
+    </v-row>
 
     <!-- Dialog se existir erros no pedido à API -->
     <v-dialog v-model="erroPedido" width="80%" hide-overlay>
@@ -27,33 +244,16 @@ export default {
       erros: [],
       erroPedido: false,
       dialogTipologias: false,
-      infoPedido: [
-        {
-          campo: "Designação",
-          conteudo: this.p.objeto.dados.ae.fundo,
-          cor: null,
-        },
-        {
-          campo: "Internacional",
-          conteudo: this.p.objeto.dados.ae,
-          cor: null,
-        },
-        { campo: "SIOE", conteudo: this.p.objeto.dados.ae.fundo, cor: null },
-        {
-          campo: "Tipologias",
-          conteudo: this.p.objeto.dados.ae.legislacao,
-          cor: null,
-        },
-        {
-          campo: "Data Extinção",
-          conteudo: this.p.objeto.dados.ae.fundo,
-          cor: null,
-        },
+      search: "",
+      cabecalho: [
+        { text: "Código", align: "left", sortable: false, value: "codigo" },
+        { text: "Título", align: "left", value: "titulo" },
+        { text: "Data de Contagem", align: "center", value: "dataContagem" },
+        { text: "Natureza de Intervenção", align: "center", value: "ni" }
       ],
-      headersTipologias: [
-        { text: "Sigla", value: "sigla", class: "subtitle-1" },
-        { text: "Designação", value: "designacao", class: "subtitle-1" },
-      ],
+      footer_props: {
+        "items-per-page-text": "Mostrar"
+      }
     };
   },
 
@@ -83,7 +283,7 @@ export default {
 
         this.$router.go(-1);
       } catch (e) {
-        console.log("e :", e);
+        //console.log("e :", e);
       }
     },
 
@@ -91,10 +291,7 @@ export default {
       try {
         let pedido = JSON.parse(JSON.stringify(this.p));
 
-        let numeroErros = await this.validarEntidade(
-          pedido.objeto.acao,
-          pedido.objeto.dados
-        );
+        var numeroErros = 0
 
         if (numeroErros > 0) {
           this.erroPedido = true;
@@ -109,7 +306,7 @@ export default {
             }
           }
 
-          await this.$request("post", "/entidades", pedido.objeto.dados);
+          await this.$request("post", "/autosEliminacao",  {auto: pedido.objeto.dados.ae});
 
           const estado = "Validado";
 
@@ -138,7 +335,7 @@ export default {
           mensagem: "Ocorreu um erro ao aceder à ontologia.",
         });
         this.erroPedido = true;
-        console.log("e :", e);
+        //console.log("e :", e);
       }
     },
 
