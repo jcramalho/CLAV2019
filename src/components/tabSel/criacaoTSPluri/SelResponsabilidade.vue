4347a816b278da2f323c57229227dcc89b530e0e
José Carlos Ramalho <jcr@iMac-de-iMac-2.lan>
2020-05-19 00:00:47 +0100

    CRiação de TSPluri: implementei o aplica a todos para selecionar donos e participantes.

--- src/components/tabSel/criacaoTSPluri/SelResponsabilidade.vue
+++ src/components/tabSel/criacaoTSPluri/SelResponsabilidade.vue
@@ -7,6 +7,27 @@
           {{ p.codigo }} - {{ p.titulo }}
         </v-card-title>
         <v-card-text>
+          <v-row>
+            <v-col cols="2">
+              <span class="table-header subtitle-2 font-weight-bold">Aplica a todos</span>
+            </v-col>
+            <v-col cols="2">
+              <span class="table-header subtitle-2 font-weight-bold">Dono</span>
+              <v-checkbox color="indigo darken-4" v-model="todosDonos"></v-checkbox>
+            </v-col>
+            <v-col cols="8">
+              <span class="table-header subtitle-2 font-weight-bold">Participante</span>
+              <v-radio-group row v-model="todosParticipantes">
+                <v-radio label="Não part." value="NP"></v-radio>
+                  <v-radio 
+                    v-for="p in participacao"
+                    :key="p.idtermo" 
+                    :label="p.termo.substring(0,3)" 
+                    :value="p.termo"
+                    class="caption"></v-radio>
+                  </v-radio-group>
+            </v-col>
+          </v-row>
           <v-data-table
             :items="p.entidades"
             :headers="headers"
@@ -54,6 +75,8 @@ export default {
     return {
       dialog: false,
       participacao: [],
+      todosDonos: false,
+      todosParticipantes: "NP",
 
       headers: [
           { text: 'Entidade', value: 'searchField', width: "40%", class: ["table-header", "subtitle-2", "font-weight-bold"], sortable: false},
@@ -65,6 +88,17 @@ export default {
       "items-per-page-options": [10, 20, 50, 100, -1],
       "items-per-page-all-text": "Todos"
     }
+    }
+  },
+
+  watch: {
+    todosDonos: function () {
+      for(let i=0; i < this.p.entidades.length; i++)
+        this.p.entidades[i].dono = this.todosDonos;
+    },
+    todosParticipantes: function () {
+      for(let i=0; i < this.p.entidades.length; i++)
+        this.p.entidades[i].participante = this.todosParticipantes;
     }
   },
 
