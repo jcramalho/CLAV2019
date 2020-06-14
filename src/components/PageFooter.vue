ec53c42eabeeee1c7e79e76947e4c5cf4081d6e7
José Carlos Ramalho <jcr@iMac-de-iMac.lan>
2020-06-14 15:33:46 +0100

    Versão

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -11,7 +11,7 @@
     </v-toolbar-title>
     <v-spacer></v-spacer>
 
-    <div class="body-2">Versão: 2020-06-07</div>
+    <div class="body-2">Versão: 2020-06-14</div>
 
     <v-spacer></v-spacer>
 
