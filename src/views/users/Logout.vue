78423c838be9a87f64aee56b58af6a09430fa22e
jcm300 <zeclmartins@gmail.com>
2020-07-20 13:52:24 +0100

    Simplificação do código no logout

--- src/views/users/Logout.vue
+++ src/views/users/Logout.vue
@@ -18,15 +18,9 @@ export default {
     this.$store.commit("guardaNomeUtilizador", "");
     this.$store.commit("guardaEntidade", "");
 
-    //se já está na página inicial (home)
-    if (this.$route.path == "/") {
-      //faz reload da página para atualizar os componentes que dependem do nível do utilizador
-      this.$router.go();
-    } else {
-      this.$router.push(
-        "/?sucesso=" + encodeURIComponent("Logout efetuado com sucesso!")
-      );
-    }
+    this.$router.push(
+      "/?sucesso=" + encodeURIComponent("Logout efetuado com sucesso!")
+    );
   }
 };
 </script>
