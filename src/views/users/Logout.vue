2c0b417f52e42d4bff46cbe69e718d4065f8230c
jcm300 <zeclmartins@gmail.com>
2020-07-20 13:48:17 +0100

    Adicionada rota de logout para o Auth.gov

---
+++ src/views/users/Logout.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-col style="text-align:center;">
+    <v-progress-circular
+      indeterminate
+      size="100"
+      width="10"
+      color="indigo accent-4"
+    />
+  </v-col>
+</template>
+
+<script>
+export default {
+  created() {
+    // this.$store.state.name = '';
+    // this.$store.state.token = '';
+    this.$store.commit("guardaTokenUtilizador", "");
+    this.$store.commit("guardaNomeUtilizador", "");
+    this.$store.commit("guardaEntidade", "");
+
+    //se já está na página inicial (home)
+    if (this.$route.path == "/") {
+      //faz reload da página para atualizar os componentes que dependem do nível do utilizador
+      this.$router.go();
+    } else {
+      this.$router.push(
+        "/?sucesso=" + encodeURIComponent("Logout efetuado com sucesso!")
+      );
+    }
+  }
+};
+</script>
