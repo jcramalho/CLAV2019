56d938ccbfcd3952754cf7418e9e7cd6986cbc79
José Carlos Ramalho <jcr@iMac-de-iMac.lan>
2020-07-29 14:39:00 +0100

    Alterei o show para poder fazer debug ao lançamento de pendentes e pedidos.

--- src/components/pendentes/consulta/ShowPendente.vue
+++ src/components/pendentes/consulta/ShowPendente.vue
@@ -3,8 +3,8 @@
     <v-card-title class="pa-2 indigo darken-4 title white--text">
         Consulta do objeto do pendente: {{ p._id }} ({{p.acao}}-{{p.tipo}})</v-card-title>
     <v-card-text>
-      <ShowTSOrg v-if="p.tipo == 'TS Organizacional'" :p="p" />
-      <ShowTSPluri v-else-if="p.tipo == 'TS Pluriorganizacional'" :p="p" />
+      <!--ShowTSOrg v-if="p.tipo == 'TS Organizacional'" :p="p" /-->
+      <ShowTSPluri v-if="p.tipo == 'TS Pluriorganizacional'" :p="p" />
       <ShowClasse v-else-if="p.tipo == 'Classe'" :p="p" />  
       <ShowDefault v-else :p="p" />
     </v-card-text>
@@ -25,7 +25,7 @@ export default {
 
   components: {
     ShowClasse,
-    ShowTSOrg,
+    //ShowTSOrg,
     ShowTSPluri,
     ShowDefault
   },
