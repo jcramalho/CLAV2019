919fb240a41e57ebede85d13e953bbff31f2f61b
ruiaraujo012 <ruiaraujo012@gmail.com>
2020-07-29 17:34:23 +0100

    Alteração da interface de submissão de pedidos
    Agora recebe como parâmetro o numero do pedido passado pelo uri

--- src/views/pedidos/PedidosSubmissao.vue
+++ src/views/pedidos/PedidosSubmissao.vue
@@ -1,5 +1,5 @@
 <template>
-  <SubmissaoPedido />
+  <SubmissaoPedido :idp="$route.params.idPedido" />
 </template>
 
 <script>
@@ -7,7 +7,7 @@ import SubmissaoPedido from "@/components/pedidos/SubmissaoPedido.vue"; // @ is 
 
 export default {
   components: {
-    SubmissaoPedido
-  }
+    SubmissaoPedido,
+  },
 };
 </script>
