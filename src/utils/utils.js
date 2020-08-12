44d4ca55a2f05e10c73575421ad655d6a243d0ed
ruiaraujo012 <ruiaraujo012@gmail.com>
2020-08-12 11:12:11 +0100

    Workflow: Alterção da key "Data" para "Data do Diploma" para as legislação

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -77,6 +77,10 @@ export function mapKeys(key) {
       descricao = "Data de Revogação";
       break;
 
+    case "data":
+      descricao = "Data do Diploma";
+      break;
+
     case "numero":
       descricao = "Número";
       break;
