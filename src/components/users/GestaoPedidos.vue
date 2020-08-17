889ad164cac065d69f4d372894cf0d9bf5f0970c
Rui Araújo <ruiaraujo012@gmail.com>
2020-08-17 19:34:00 +0100

    Nova interface com os pedidos criados pelo utilizador atual.

---
+++ src/components/users/GestaoPedidos.vue
@@ -0,0 +1,192 @@
+<template>
+  <v-card shaped class="ma-8">
+    <v-card-title class="indigo darken-4 white--text" dark>
+      Lista de pedidos submetidos
+      <v-spacer />
+      <v-chip color="indigo accent-4" text-color="white">
+        <b>Total de pedidos: {{ pedidos.length }}</b>
+      </v-chip>
+    </v-card-title>
+
+    <v-card-text>
+      <v-card>
+        <v-card-title>
+          <v-text-field
+            v-model="procurar"
+            append-icon="search"
+            label="Procurar pedido"
+            single-line
+            hide-details
+            filled
+          />
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="pedidos"
+          :search.sync="procurar"
+          class="elevation-1"
+          sortDesc
+          sort-by="codigo"
+          :custom-sort="ordenaTabela"
+          :footer-props="footerProps"
+          :items-per-page="5"
+          @click:row="showPedido($event)"
+        >
+          <template v-slot:no-data>
+            <v-alert type="error" width="50%" class="m-auto mb-2 mt-2" outlined>
+              Não existem pedidos neste estado...
+            </v-alert>
+          </template>
+
+          <template v-slot:no-results>
+            <v-alert type="info" width="50%" class="m-auto mb-2 mt-2" outlined>
+              Sem resultados para "<strong>{{ procurar }}</strong
+              >".
+            </v-alert>
+          </template>
+
+          <template v-slot:item.estado="{ item }">
+            <v-chip v-if="item.estado === 'Validado'" outlined color="green">
+              {{ item.estado }}
+              <v-icon right>assignment_turned_in</v-icon>
+            </v-chip>
+
+            <v-chip
+              v-else-if="item.estado === 'Devolvido'"
+              outlined
+              color="red"
+            >
+              {{ item.estado }}
+              <v-icon right>assignment_late</v-icon>
+            </v-chip>
+            <v-chip v-else outlined color="orange">
+              {{ item.estado }}
+              <v-icon right>assignment</v-icon>
+            </v-chip>
+          </template>
+
+          <template v-slot:footer.page-text="props">
+            {{ props.pageStart }} - {{ props.pageStop }} de
+            {{ props.itemsLength }}
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["pedidos"],
+
+  data() {
+    return {
+      procurar: "",
+      headers: [
+        {
+          text: "Código",
+          value: "codigo",
+          class: "title",
+          sortable: true,
+          filterable: true,
+        },
+        {
+          text: "Tipo",
+          value: "tipo",
+          class: "title",
+          sortable: true,
+          filterable: true,
+        },
+        {
+          text: "Data",
+          value: "data",
+          class: "title",
+          sortable: true,
+          filterable: true,
+        },
+        {
+          text: "Estado",
+          value: "estado",
+          class: "title",
+          sortable: true,
+          filterable: true,
+        },
+      ],
+      footerProps: {
+        "items-per-page-text": "Pedidos por página",
+        "items-per-page-options": [5, 10, -1],
+        "items-per-page-all-text": "Todos",
+      },
+    };
+  },
+
+  methods: {
+    ordenaTabela(items, index, isDesc) {
+      items.sort((a, b) => {
+        if (index[0] === "codigo") {
+          if (!isDesc[0]) {
+            return (
+              parseInt(b[index].split("-")[0].concat(b[index].split("-")[1])) -
+              parseInt(a[index].split("-")[0].concat(a[index].split("-")[1]))
+            );
+          } else {
+            return (
+              parseInt(a[index].split("-")[0].concat(a[index].split("-")[1])) -
+              parseInt(b[index].split("-")[0].concat(b[index].split("-")[1]))
+            );
+          }
+        } else if (index[0] === "data") {
+          if (!isDesc[0]) {
+            const dataA = a[index]
+              .split("-")
+              .reverse()
+              .join("");
+
+            const dataB = b[index]
+              .split("-")
+              .reverse()
+              .join("");
+
+            return dataB.localeCompare(dataA);
+          } else {
+            const dataA = a[index]
+              .split("-")
+              .reverse()
+              .join("");
+
+            const dataB = b[index]
+              .split("-")
+              .reverse()
+              .join("");
+
+            return dataA.localeCompare(dataB);
+          }
+        } else {
+          if (
+            typeof a[index] !== "undefined" &&
+            typeof b[index] !== "undefined"
+          ) {
+            if (!isDesc[0]) {
+              return a[index]
+                .toLowerCase()
+                .localeCompare(b[index].toLowerCase());
+            } else {
+              return b[index]
+                .toLowerCase()
+                .localeCompare(a[index].toLowerCase());
+            }
+          }
+        }
+      });
+
+      return items;
+    },
+
+    showPedido(pedido) {
+      // TODO: Alterar isto para um componente mais simples
+      // (apenas informações básicas do pedido)
+      this.$router.push("/pedidos/" + pedido.codigo);
+    },
+  },
+};
+</script>
