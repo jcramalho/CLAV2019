8a3f3b695440bfeab68f605d7cf994a399151c62
Rui Araújo <ruiaraujo012@gmail.com>
2020-09-17 19:27:34 +0100

    Workflow: Alguns melhoramentos no pdf (Não terminado)

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -3,93 +3,59 @@ import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const pdf = {
-  content: [
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-    "Teste",
-  ],
+  header: "simple text",
 
+  footer: {
+    columns: ["Left part", { text: "Right part", alignment: "right" }],
+  },
+};
+
+const content = [];
+const styles = {
   defaultStyle: {
     fontSize: 15,
     bold: false,
   },
 };
 
+const gerarHeaderConteudo = (dados) => {
+  const header = {
+    columns: [
+      { text: dados.tipoPedido, style: "tipoPedido" },
+      { text: dados.estadoPedido, style: "estadoPedido" },
+    ],
+  };
+
+  styles.tipoPedido = {
+    fontSize: 18,
+    bold: true,
+  };
+
+  styles.estadoPedido = {
+    fontSize: 18,
+    bold: true,
+    alignment: "right",
+    color: "green",
+    // background: "#C8E6C9",
+    background: "#E8F5E9",
+    decoration: "underline",
+    decorationColor: "green",
+  };
+
+  content.push(header);
+};
+
 const guardarPDF = (nome) => {
+  pdf.content = content;
+  pdf.styles = styles;
+
   pdfMake.createPdf(pdf).download(`relatorio-${nome}`);
 };
 
 export const gerarPDF = (relatorio) => {
   console.log("relatorio", relatorio);
+
+  gerarHeaderConteudo(relatorio);
 
   guardarPDF(relatorio.numeroPedido);
 };
