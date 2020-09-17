1187370f61ecc34f69d103be718a6dbf344d872e
José Carlos Ramalho <jcr@iMac-de-iMac-2.lan>
2020-09-17 23:12:41 +0100

    Configs.

--- makedocker.sh
+++ makedocker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker build . -t clav2020
 echo 'Tagging'
-docker tag clav2020:latest zzglider/clav2020:Set04
+docker tag clav2020:latest zzglider/clav2020:Set17
 echo 'Pushing'
-docker push zzglider/clav2020:Set04
+docker push zzglider/clav2020:Set17
 echo 'Terminado'
-echo 'Id da imagem no DHub: zzglider/clav2020:Set04'
+echo 'Id da imagem no DHub: zzglider/clav2020:Set17'
 
