4176a4d10031b1123ab2d957c682444c29d694e3
Ricardo Canela <ricardocanela.ns@gmail.com>
2021-02-20 00:51:37 +0000

    file consulta classes

---
+++ src/components/generic/Voltar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn @click="$router.go(-1)" rounded class="white--text px-2 clav-linear-background">
+    <unicon
+      name="arrow-back-icon"
+      width="20"
+      height="20"
+      viewBox="0 0 20.71 37.261"
+      fill="#ffffff"
+    />
+    <p class="ml-2">Voltar</p>
+  </v-btn>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
