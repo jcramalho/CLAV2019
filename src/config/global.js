0b1e865410bd2aca95d9ba32f8ef49bf986bde8c
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2021-04-17 08:10:43 +0100

    Nova versão para deployment.

--- src/config/global.js
+++ src/config/global.js
@@ -10,6 +10,6 @@ const dominioDefault = "http://localhost:7779";
 const dominio = process.env.VUE_APP_API_URL || dominioDefault;
 const apiVersion = process.env.VUE_APP_API_VERSION || "v2";
 
-module.exports.interfaceVersion = process.env.VUE_APP_INTERFACE_VERSION || "2021-04-08";
+module.exports.interfaceVersion = process.env.VUE_APP_INTERFACE_VERSION || "2021-04-17";
 
 module.exports.host = dominio + "/" + apiVersion;
