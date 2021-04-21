3179ae1bc04222aae8f1680662345965238018d0
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2021-04-21 10:35:59 +0100

    Nova versão para deployment.

--- src/config/global.js
+++ src/config/global.js
@@ -10,6 +10,6 @@ const dominioDefault = "http://clav.di.uminho.pt"; //para o nginx proxy quando a
 const dominio = process.env.VUE_APP_API_URL || dominioDefault;
 const apiVersion = process.env.VUE_APP_API_VERSION || "v2";
 
-module.exports.interfaceVersion = process.env.VUE_APP_INTERFACE_VERSION || "2021-04-18";
+module.exports.interfaceVersion = process.env.VUE_APP_INTERFACE_VERSION || "2021-04-21";
 
 module.exports.host = dominio + "/" + apiVersion;
