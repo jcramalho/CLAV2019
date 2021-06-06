81f9756effbe05a411ff9913616568be66d4b2f6
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2021-06-06 12:51:43 +0100

    Bug na criação de AE, adição de classes com df = CP, corrigido: vai-se buscar a justificação à LC:

--- src/components/autosEliminacao/criacao/DialogZonaControlo.vue
+++ src/components/autosEliminacao/criacao/DialogZonaControlo.vue
@@ -362,12 +362,15 @@ export default {
         else this.prazo = (c[0].pca.valores || "") + " Anos";
         if (c[0].pca.notas) this.notasPCA = c[0].pca.notas;
         if (c[0].df.nota) this.notaDF = c[0].df.nota;
+        
         if (
           (this.tipo == "TS_LC" || this.tipo == "PGD_LC") &&
-          c[0].df.valor == "CP" &&
-          c[0].df.justificacao
+          c[0].df.valor == "CP" 
         )
-          this.justificaDF = c[0].df.justificacao.data.map((just) => {
+          // Para DF = CP vou buscar a justificação do DF
+          var classeLC = await this.$request("get", "/classes/c" + c[0].codigo);
+          
+          this.justificaDF = classeLC.data.df.justificacao.map((just) => {
             return just.conteudo;
           });
         
