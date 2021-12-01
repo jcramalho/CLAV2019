cdfdde69e56093fd5e5c62ad6926c6593dc47587
Ricardo Canela <ricardocanela.ns@gmail.com>
2021-12-01 17:18:50 +0000

    design apagar coleções

--- src/views/gestao/ApagarColecoes.vue
+++ src/views/gestao/ApagarColecoes.vue
@@ -1,52 +1,39 @@
 <template>
-  <v-container fluid>
-    <v-row row wrap justify-center>
-      <v-col cols="12">
-        <v-card>
-          <v-toolbar color="indigo darken-4" dark>
-            <v-toolbar-title>Apagar Coleções</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <div class="subtitle-1 font-weight-black">
-              Aqui pode <span style="color:red">apagar</span> a coleção
-              selecionada. Esta operação
-              <span style="color:red">
-                elimina todos os documentos da coleção
-              </span>
-              escolhida.
-            </div>
-            <br />
-            <v-form ref="form">
-              <v-autocomplete
-                :items="eliminacoesDisponiveis"
-                label="Coleção a apagar"
-                v-model="colecao"
-                :rules="regraColecao"
-                required
-              >
-              </v-autocomplete>
-            </v-form>
+  <v-card flat class="pa-3">
+    <v-card-title class="clav-content-title-1" dark> Apagar Coleções </v-card-title>
+    <v-card-text>
+      <div class="subtitle-1">
+        Aqui pode
+        <span style="color: var(--v-error-base)" class="font-weight-black">apagar</span> a
+        coleção selecionada. Esta operação
+        <span style="color: var(--v-error-base)" class="font-weight-black">
+          elimina todos os documentos da coleção
+        </span>
+        escolhida.
+      </div>
+      <br />
+      <v-form ref="form">
+        <v-autocomplete
+          :items="eliminacoesDisponiveis"
+          label="Coleção a apagar"
+          v-model="colecao"
+          :rules="regraColecao"
+          required
+        >
+        </v-autocomplete>
+      </v-form>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="red" dark @click="cancelar">
-                Cancelar
-              </v-btn>
-              <v-btn color="indigo darken-4" dark @click="apagar">
-                Apagar
-              </v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-alert :value="text != ''" :type="alertType">
-          {{ text }}
-        </v-alert>
-      </v-col>
-    </v-row>
-  </v-container>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="error" dark rounded @click="cancelar"> Cancelar </v-btn>
+        <v-btn color="primary" dark rounded @click="apagar"> Apagar </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card-text>
+    <v-alert :value="text != ''" :type="alertType">
+      {{ text }}
+    </v-alert>
+  </v-card>
 </template>
 
 <script>
@@ -58,12 +45,12 @@ export default {
         { text: "Logs", value: "logs" },
         { text: "Logs Agregados", value: "logsAgregados" },
         { text: "Pedidos", value: "pedidos" },
-        { text: "Trabalhos Pendentes", value: "pendentes" }
+        { text: "Trabalhos Pendentes", value: "pendentes" },
       ],
       colecao: "",
-      regraColecao: [v => !!v || "Coleção a apagar é obrigatório."],
+      regraColecao: [(v) => !!v || "Coleção a apagar é obrigatório."],
       text: "",
-      alertType: "success"
+      alertType: "success",
     };
   },
 
@@ -86,7 +73,7 @@ export default {
           this.alertType = "error";
         }
       }
-    }
-  }
+    },
+  },
 };
 </script>
