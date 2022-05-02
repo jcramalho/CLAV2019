276200eb79784458fc4318bd06b7c76f8b082a5c
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2022-05-02 16:56:48 +0100

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
+  runtimeCompiler: true,
+  devServer: {
+    port: '3000',
+    proxy: {
+      '/engine-rest': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        pathRewrite: {
+        }
+      }
+    }
+  }
 };
 
