518a661d27997620f3b2578d01b089f8dd65e7b2
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2022-10-12 07:01:44 +0100

    Nova versão.

--- makedocker.sh
+++ makedocker.sh
@@ -2,9 +2,8 @@
 npm run build
 docker build . -t clav2022
 echo 'Tagging'
-docker tag clav2022:latest zzglider/clav2022:Jan06
+docker tag clav2022:latest zzglider/clav2022:Oct12
 echo 'Pushing'
-docker push zzglider/clav2022:Jan06
+docker push zzglider/clav2022:Oct12
 echo 'Terminado'
-echo 'Id da imagem no DHub: zzglider/clav2022:Jan06'
-
+echo 'Id da imagem no DHub: zzglider/clav2022:Oct12'
